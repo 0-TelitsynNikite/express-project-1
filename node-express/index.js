6c2a4a30bb3f8acc73cff2a412303f230831230f
Телицын Никита <dmtvn.labs@yandex.ru>
2021-12-15 00:14:02 +0300

    commit

--- node-express/index.js
+++ node-express/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const app = express();
+const homeRoutes = require('./routes/home');
+const addRoutes = require('./routes/add');
+const coursesRoutes = require('./routes/courses');
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs',
-})
+});
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
@@ -14,28 +17,12 @@ app.set('views', 'views');
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}))
+app.use('/', homeRoutes);
+app.use('/add', addRoutes);
+app.use('/courses', coursesRoutes);
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Main page',
-        isHome: true
-    })
-});
-
-app.get('/add', (req, res) => {
-    res.render('add', {
-        title: 'Add page',
-        isAdd: true
-    })
-});
-
-app.get('/courses', (req, res) => {
-    res.render('courses', {
-        title: 'Courses page',
-        isCourses: true
-    })
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
