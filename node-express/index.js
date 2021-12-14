eee9c4142a4d4553edff7e35d87e5057bce1d9a6
Телицын Никита <dmtvn.labs@yandex.ru>
2021-12-14 17:24:38 +0300

    third commit

--- node-express/index.js
+++ node-express/index.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const path = require('path');
-
+const exphbs = require('express-handlebars');
 const app = express();
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs',
+})
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+    res.render('index', {
+        title: 'Main page',
+        isHome: true
+    })
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'))
+app.get('/add', (req, res) => {
+    res.render('add', {
+        title: 'Add page',
+        isAdd: true
+    })
+});
+
+app.get('/courses', (req, res) => {
+    res.render('courses', {
+        title: 'Courses page',
+        isCourses: true
+    })
 });
 
 app.listen(PORT, () => {
